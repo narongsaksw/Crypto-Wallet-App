a6db66d6951391616e65dfa71321cc436435a102
sscarry2 <earth.narongsak@gmail.com>
2021-05-31 01:48:02 +0700

    header home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,143 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {View, Text} from 'react-native';
+import {useFocusEffect} from '@react-navigation/native';
 
+import {connect} from 'react-redux';
+import {getHoldings, getCoinMarket} from '../stores/market/marketActions';
+
+import {BalanceInfo, IconTextButton} from '../components';
 import {MainLayout} from './';
+import {SIZES, COLORS, FONTS, dummyData, icons} from '../constants';
 
-const Home = () => {
+const Home = ({getHoldings, getCoinMarket, myHoldings, coins}) => {
+  useFocusEffect(
+    useCallback(() => {
+      getHoldings((holdings = dummyData.holdings));
+      getCoinMarket();
+    }, []),
+  );
+
+  let totalWallet = myHoldings.reduce((a, b) => a + (b.total || 0), 0);
+  let valueChange = myHoldings.reduce(
+    (a, b) => a + (b.holding_value_change_7d || 0),
+    0,
+  );
+  let percChange = (valueChange / (totalWallet - valueChange)) * 100;
+
+  const renderWalletInfoSection = () => {
+    return (
+      <View
+        style={{
+          paddingHorizontal: SIZES.padding,
+          borderBottomRightRadius: 25,
+          borderBottomLeftRadius: 25,
+          backgroundColor: COLORS.gray,
+        }}>
+        <BalanceInfo
+          title="Your Wallet"
+          displayAmount={totalWallet}
+          changePct={percChange}
+          containerStyle={{marginTop: 50}}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: 30,
+            marginBottom: -15,
+            paddingHorizontal: SIZES.radius,
+          }}>
+          <IconTextButton
+            label="Transfer"
+            icon={icons.send}
+            containerStyle={{
+              flex: 1,
+              height: 40,
+              marginRight: SIZES.radius,
+            }}
+            onPress={() => console.log('Transfer')}
+          />
+          <IconTextButton
+            label="Withdraw"
+            icon={icons.withdraw}
+            containerStyle={{
+              flex: 1,
+              height: 40,
+            }}
+            onPress={() => console.log('Transfer')}
+          />
+        </View>
+      </View>
+    );
+  };
+
   return (
     <MainLayout>
-      <View>
-        <Text>Home</Text>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.black,
+        }}>
+        {/* Header Wallet Info */}
+        {renderWalletInfoSection()}
       </View>
     </MainLayout>
   );
 };
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    myHoldings: state.marketReducer.myHoldings,
+    coins: state.marketReducer.coins,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getHoldings: (
+      holdings,
+      currency,
+      coinList,
+      orderBy,
+      sparkline,
+      priceChangePerc,
+      perPage,
+      page,
+    ) => {
+      return dispatch(
+        getHoldings(
+          holdings,
+          currency,
+          coinList,
+          orderBy,
+          sparkline,
+          priceChangePerc,
+          perPage,
+          page,
+        ),
+      );
+    },
+    getCoinMarket: (
+      currency,
+      coinList,
+      orderBy,
+      sparkline,
+      priceChangePerc,
+      perPage,
+      page,
+    ) => {
+      return dispatch(
+        getCoinMarket(
+          currency,
+          coinList,
+          orderBy,
+          sparkline,
+          priceChangePerc,
+          perPage,
+          page,
+        ),
+      );
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
